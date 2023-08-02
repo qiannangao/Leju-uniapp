a4138e6ffa8d8647f6ade5d96a64823d3db939c2
高倩南 <3065966250@qq.com>
2023-08-02 15:36:18 +0800

    third

--- api/baseURL.js
+++
@@ -1,9 +0,0 @@
-let baseURL='';
- if(process.env.NODE_ENV==='development'){
-	 baseURL='http://localhost'
-	 // baseURL='http://192.168.124.37'
-	  // baseURL="http://leju.bufan.cloud"
- }else if(process.env.NODE_ENV==='production'){
-	 baseURL="http://leju.bufan.cloud"
- }
- export default baseURL
